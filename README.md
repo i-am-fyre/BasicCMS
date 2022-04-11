b3c21057a709554926e59e27c8b0aff6aaa2d3ed
Fyre <fyre.homelab+github@gmail.com>
2022-04-10 20:52:02 -0500

    Add login, password change, and update readme.

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A very simple CMS page for MaNGOS projects. The purpose of this project is to provide a registration page to link to the realmd database and create new accounts.
 
+## Features
+
+- Register accounts
+- Player login
+- Administrator login
+- Change password
+
 ## Getting Started
 
 These instructions will give you a copy of the project up and running on
@@ -13,13 +20,17 @@ Run the following:
 - `sudo apt-get install libmariadb-dev python3-dev`
 - `pip install -r requirements.txt`
 
-### Installing
+### Configuration
 
-Once the pre-requisites above have been installed, do the following:
+You can set your database information in `config.ini`.
+You can select which expansions of MaNGOS the user can register for within the `register.html`.
+
+### Starting Up
+
+Once the pre-requisites above have been installed and configurations are set, do the following:
 - `export FLASK_APP=main.py`
 - `flask run`
-
-Your local server should be running at http://localhost:5000
+Your local server should then be running at http://localhost:5000
 
 ## Screenshots
 ![MangosRegister](https://user-images.githubusercontent.com/58180427/162591117-71d84e9b-f769-4d8d-a5a3-457ef0180c80.png)
--- main.py
+++ main.py
@@ -1,29 +1,71 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
+import configparser
+import MySQLdb
 import re
 from secrets import token_hex
 from hashlib import sha1
 
 app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 app.secret_key = token_hex(16)
 
-# Enter your database connnection details
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'mangos'
-app.config['MYSQL_PASSWORD'] = 'mangos'
-app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_DB'] = 'realmd'
-
 # Initialize database
-mysql = MySQL(app)
-
+mysql = MySQLdb.connect(host=config.get('Database', 'host'),
+                        user=config.get('Database', 'user'),
+                        password=config.get('Database', 'password'),
+                        port=int(config.get('Database', 'port')),
+                        database=config.get('Database', 'realmd_name'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     msg = ''
     return render_template('index.html', msg='')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        # cur = mysql.cursor()
+        cur = mysql.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM account WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data[2]
+            
+
+            # Compare Passwords
+            if sha1((username + ":" + password_candidate).upper().encode('utf-8')).hexdigest().upper() == password.upper():
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+                session['gmlevel'] = data[3]
+
+                return redirect(url_for('index'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', msg=error)
+            # Close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', msg=error)
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    [session.pop(key) for key in list(session.keys()) if key != '_flashes']
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -36,7 +78,7 @@ def register():
 
         print (expansion)
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor = mysql.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM account WHERE username = %s', (username,))
         account = cursor.fetchone()
 
@@ -47,11 +89,41 @@ def register():
         elif not username or not password or not expansion:
             msg = 'Please fill out the form!'
         else:
-            pre_hash_pass = (username + ":" + password).upper()
-            sha_pass_hash = sha1(pre_hash_pass.encode('utf-8')).hexdigest()
+            sha_pass_hash = sha1((username + ":" + password).upper().encode('utf-8')).hexdigest()
             cursor.execute('INSERT INTO account (username, sha_pass_hash, expansion) VALUES (%s, %s, %s)', (username, sha_pass_hash, expansion))
-            mysql.connection.commit()
+            mysql.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
         msg = 'Please fill out the form!'
-    return render_template('index.html', msg=msg)
+    return render_template('register.html', msg=msg)
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('profile.html', msg='')
+
+@app.route('/admin_dashboard', methods=['GET', 'POST'])
+def admin_dashboard():
+    return render_template('admin_dashboard.html', msg='')
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    old_password = request.form['old_password']
+    new_password = request.form['new_password']
+
+    if old_password and new_password:
+        cursor = mysql.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM account WHERE username = %s', (session['username'],))
+        account = cursor.fetchone()
+
+        if account:
+            if sha1((session['username'] + ":" + old_password).upper().encode('utf-8')).hexdigest().upper() == account['sha_pass_hash'].upper():
+                sha_pass_hash = sha1((session['username'] + ":" + new_password).upper().encode('utf-8')).hexdigest().upper()
+                cursor.execute('UPDATE account SET sha_pass_hash = %s, v = %s, s = %s WHERE username = %s', (sha_pass_hash, "", "", session['username']))
+                mysql.commit()
+                msg = 'Password successfully changed!'
+            else:
+                msg = 'Old password is incorrect!'
+        else:
+            msg = 'Account not found!'
+
+    return render_template('profile.html', msg='')
